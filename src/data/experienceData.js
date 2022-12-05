ba9daf819340ade8d66db394d5170fb121430699
Muhammad-Akif <muhammadakif2917@gmail.com>
2022-12-05 14:48:38 +0500

    updated experience content

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,7 +1,7 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'MERN IT SOLUTIONS',
+        company: 'Mern It Solutions',
         jobtitle: 'Software Engineer',
         startYear: 'Nov 2022',
         endYear: 'Present'
@@ -10,14 +10,21 @@ export const experienceData = [
         id: 2,
         company: 'Xtreme Web Tech',
         jobtitle: 'Full-Stack Developer',
-        startYear: 'Jun 2021',
-        endYear: 'Jan 2022'
+        startYear: 'April 2021',
+        endYear: 'Nov 2022'
     },
     {
         id: 3,
-        company: 'Learning New Technology For Everyday',
-        jobtitle: 'Self Learning',
-        startYear: '2018',
+        company: 'Smart Fusion',
+        jobtitle: 'Full-Stack Developer',
+        startYear: 'Mar 2021',
+        endYear: 'April 2022'
+    },
+    {
+        id: 4,
+        company: 'Local Market & Freelancing',
+        jobtitle: 'Full Stack Developer | DevOps Engineer',
+        startYear: '2020',
         endYear: 'Present'
     },
 ]
